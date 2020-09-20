f23786599c5ebce16f371df0a466cb5e4b0a9c71
The Shift <Mercy@Mercys-MacBook-Air.local>
2020-09-20 14:52:05 +0300

    [Path Findinng]

---
+++ sketch.js
@@ -0,0 +1,56 @@
+let cols = 5;
+let rows = 5;
+let grid = new Array(cols);
+
+let openSet = [];
+let closedSet = [];
+let start;
+let end;
+let w, h;
+
+function Spot(i, j) {
+  this.x = i;
+  this.y = j;
+  this.f = 0;
+  this.g = 0;
+  this.h = 0;
+
+  this.show = function () {
+    fill(255);
+    stroke(0);
+    rect(this.x * w, this.y * h, w, h);
+  };
+}
+
+function setup() {
+  createCanvas(400, 400);
+  console.log("A*");
+
+  w = width / cols;
+  h = height / rows;
+
+  for (let i = 0; i < grid.length; i++) {
+    grid[i] = new Array(rows);
+  }
+
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      grid[i][j] = new Spot(i, j);
+    }
+  }
+
+  start = grid[0][0];
+  end = grid[cols - 1][rows - 1];
+
+  openSet.push(start);
+  console.log(grid);
+}
+
+function draw() {
+  background(0);
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      grid[i][j].show();
+    }
+  }
+}
